2acc54ec39a4c06dffd8e59247dd3026bd30c4c0
Nguyễn Thị Thảo Hồng <75102265+NguyenThiThaoHong@users.noreply.github.com>
2022-05-03 20:50:25 +0700

    Máy tính

---
+++ calculator/Calculator.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author s
+ */
+public class Calculator extends cal {
+
+    public Calculator()
+    {
+        setSize(200,200);
+        setTitle("May tinh");
+        setVisible(true);
+        new cal();
+    }
+
+    
+    
+}
